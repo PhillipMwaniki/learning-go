ad1c38629851601aacc5a1b08f2a1e324016108c
Phillip Nzuli <mwanikiphillip@gmail.com>
2020-04-06 14:12:51 +0300

    we can now save to file

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+
+	// fmt.Println(cards.toString())
+	cards.saveToFile("cards")
 }
