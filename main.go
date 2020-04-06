d541ada4b974152f0fe2885df25957896ca5465c
Phillip Nzuli <mwanikiphillip@gmail.com>
2020-04-06 15:24:06 +0300

    we can now write n read from file

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("cards")
 
+	cards.print()
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.print()
 	// remainingCards.print()
 
 	// fmt.Println(cards.toString())
-	cards.saveToFile("cards")
+	// cards.saveToFile("cards")
 }
